d66af888976143776815c24c2bde86d22a96536c
Slava <plumsoftwareofficial@gmail.com>
2024-02-01 23:45:33 +0600

    [+] crete notification from remote message [+] added notification logo

--- app/src/main/java/ru/plumsoftware/marvel/service/firebase/FirebaseMessagingService.kt
+++ app/src/main/java/ru/plumsoftware/marvel/service/firebase/FirebaseMessagingService.kt
@@ -1,50 +1,51 @@
 package ru.plumsoftware.marvel.service.firebase
 
 import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
-import android.util.Log
+import android.media.RingtoneManager
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import ru.plumsoftware.marvel.R
 import ru.plumsoftware.marvel.ui.presentation.activity.presentation.MainActivity
 
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class FirebaseMessagingService : FirebaseMessagingService() {
 
-    private val TAG = "ru.plumsoftware.marvel.FirebaseMessagingService"
-
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Log.d(TAG, "From: ${remoteMessage.from}")
-
         if (remoteMessage.data.isNotEmpty()) {
-            val values: MutableCollection<String> = remoteMessage.data.values
-            val data: Map<String, String> = remoteMessage.data
 
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.putExtra("hero_id", remoteMessage.data["hero_id"]?.toInt())
 
-            for ((key, value) in data) {
-                Log.d(TAG, "Message data payload: $key, $value")
+            val pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            val defaultSoundUri =
+                RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-                if (key == "hero_page") {
-                    val intent = Intent(this, MainActivity::class.java)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    intent.putExtra("hero_id", value)
-                    startActivity(intent)
-                }
-            }
+            val notificationBuilder = NotificationCompat.Builder(
+                this,
+                "ru.plumsoftware.marvel.FirebaseMessagingService"
+            )
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle(remoteMessage.notification?.title)
+                .setContentText(if (remoteMessage.notification?.body != null) remoteMessage.notification?.body else "")
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
 
-            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
-            Log.d(TAG, "Message values payload: $values")
-
-        }
-    }
-
-    override fun handleIntent(intent: Intent?) {
-        super.handleIntent(intent)
-
-        if (intent?.action == "hero_page") {
-            val mainIntent = Intent(this, MainActivity::class.java)
-            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            intent.putExtra("hero_id", intent.getIntExtra("hero_id", 1009268))
-            startActivity(mainIntent)
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(0, notificationBuilder.build())
         }
     }
 }
